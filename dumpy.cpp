49abcc8354edf9c0aad0f42914314bf82684ab2a
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2023-02-13 14:37:31 -0800

    Update dumpy.cpp

--- dumpy.cpp
+++ dumpy.cpp
@@ -2,21 +2,13 @@
 #include <fstream>
 #include <vector>
 #include <string>
-#include <cstring>
 #include <experimental/filesystem>
 
 namespace fs = std::experimental::filesystem;
 
-bool is_pe_file(const char* buffer, const size_t size) {
-    const char pe_magic[] = { 'M', 'Z' };
-    const size_t pe_magic_size = sizeof(pe_magic);
-    if (size < pe_magic_size) {
-        return false;
-    }
-    return std::memcmp(buffer, pe_magic, pe_magic_size) == 0;
-}
+const uint64_t MAX_FILE_SIZE = 100 * 1024 * 1024; // 100 MB
 
-void extract_pe_files(const std::string& input_path, const std::string& output_path) {
+void extract_executables(const std::string& input_path, const std::string& output_path) {
     std::ifstream input_file(input_path, std::ios::binary);
     if (!input_file) {
         std::cerr << "Failed to open input file: " << input_path << std::endl;
@@ -30,19 +22,25 @@ void extract_pe_files(const std::string& input_path, const std::string& output_p
     const size_t size = buffer.size();
 
     size_t pos = 0;
+    int count = 0;
     while (pos < size) {
-        if (is_pe_file(&data[pos], size - pos)) {
-            const std::string filename = (fs::path(output_path) / (std::to_string(pos) + ".exe")).generic_string();
+        const char* file_data = &data[pos];
+        size_t file_size = size - pos;
+        std::string filename = output_path + std::to_string(count) + ".exe";
+        if (file_size > MAX_FILE_SIZE) {
+            std::cerr << "Skipping file: " << filename << " (file too large)" << std::endl;
+        } else {
             std::ofstream output_file(filename, std::ios::binary);
             if (output_file) {
-                output_file.write(&data[pos], size - pos);
+                output_file.write(file_data, file_size);
                 output_file.close();
-                std::cout << "Extracted PE file: " << filename << std::endl;
+                std::cout << "Extracted file: " << filename << std::endl;
             } else {
                 std::cerr << "Failed to open output file: " << filename << std::endl;
             }
         }
-        pos++;
+        count++;
+        pos += file_size;
     }
 }
 
@@ -53,6 +51,6 @@ int main(int argc, char* argv[]) {
     }
     const std::string input_path = argv[1];
     const std::string output_path = argv[2];
-    extract_pe_files(input_path, output_path);
+    extract_executables(input_path, output_path);
     return 0;
 }
