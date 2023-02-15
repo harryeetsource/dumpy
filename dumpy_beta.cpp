46620307027cbd3c013c59e83c126cdc6241b3b1
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2023-02-14 16:33:43 -0800

    Update dumpy_beta.cpp

--- dumpy_beta.cpp
+++ dumpy_beta.cpp
@@ -1,75 +1,114 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <string>
 #include <vector>
-#include <cstring>
 #include <thread>
 #include <mutex>
-#include <queue>
 #include <condition_variable>
+#include <queue>
 #include <algorithm>
 
 #include <cstdint>
-#include <cstdio>
+#include <cstring>
 
-const uint16_t IMAGE_FILE_MACHINE_I386 = 0x014c;     // Intel x86 machine
-const uint16_t IMAGE_FILE_MACHINE_AMD64 = 0x8664;    // x64 machine
+#include <windows.h>
 
-// Structure to hold information about an executable section
-struct section_info_t {
-    uint32_t section_offset;
-    uint32_t section_size;
+// Threadpool implementation
+class ThreadPool {
+public:
+    explicit ThreadPool(size_t num_threads) {
+        for (size_t i = 0; i < num_threads; ++i) {
+            threads.emplace_back([this] {
+                for (;;) {
+                    std::function<void()> task;
+                    {
+                        std::unique_lock<std::mutex> lock{mutex};
+                        condition.wait(lock, [this] { return stop || !tasks.empty(); });
+                        if (stop && tasks.empty()) return;
+                        task = std::move(tasks.front());
+                        tasks.pop();
+                    }
+                    task();
+                }
+            });
+        }
+    }
+
+    ~ThreadPool() {
+        {
+            std::unique_lock<std::mutex> lock{mutex};
+            stop = true;
+        }
+        condition.notify_all();
+        for (auto &thread : threads) {
+            thread.join();
+        }
+    }
+
+    template<typename F, typename... Args>
+    void enqueue(F &&f, Args &&... args) {
+        {
+            std::unique_lock<std::mutex> lock{mutex};
+            tasks.emplace([f = std::forward<F>(f), args = std::make_tuple(std::forward<Args>(args)...)]() {
+                std::apply(f, args);
+            });
+        }
+        condition.notify_one();
+    }
+
+private:
+    std::vector<std::thread> threads;
+    std::queue<std::function<void()>> tasks;
+    std::mutex mutex;
+    std::condition_variable condition;
+    bool stop = false;
 };
 
-// Find the offsets of all valid PE headers in the given buffer
-void find_pe_offsets(const std::vector<char>& buffer, size_t buffer_size, std::vector<uint32_t>& pe_offsets) {
-    const char* buffer_start = buffer.data();
-    const char* buffer_end = buffer_start + buffer_size - 4;
-    for (const char* p = buffer_start; p < buffer_end; p++) {
-        if (memcmp(p, "MZ\0\0", 4) == 0) {
-            const char* pe_offset_ptr = p + *(uint32_t*)&p[0x3c];
-            if (pe_offset_ptr < buffer_end && memcmp(pe_offset_ptr, "PE\0\0", 4) == 0) {
-                uint32_t pe_offset = static_cast<uint32_t>(pe_offset_ptr - buffer_start);
-                pe_offsets.push_back(pe_offset);
+// Check for valid DOS header
+bool validate_dos_header(const char *buffer) {
+    // Check for valid MZ signature
+    if (memcmp(buffer, "MZ", 2) != 0) {
+        return false;
+    }
+
+    // Check for valid PE offset
+    uint32_t pe_offset = *reinterpret_cast<const uint32_t *>(buffer + 0x3c);
+    if (pe_offset >= 0x1000) {
+        return false;
+    }
+
+    return true;
+}
+
+// Check for valid PE signature
+bool validate_pe_signature(const char *buffer, uint32_t size) {
+    if (size < 0x1000) {
+        return false;
+    }
+
+    // Check for valid PE signature
+    if (memcmp(buffer + *reinterpret_cast<const uint32_t *>(buffer + 0x3c), "PE\0\0", 4) != 0) {
+        return false;
+    }
+
+    return true;
+}
+
+// Find the offsets of all PE files in the memory dump
+void find_pe_offsets(const std::vector<char>& data, size_t size, std::vector<uint32_t>& pe_offsets) {
+    const char* buffer = &data[0];
+    size_t max_offset = size - 0x1000;
+    for (size_t i = 0; i < max_offset; ++i) {
+        if (validate_dos_header(buffer + i)) {
+            uint32_t pe_offset = *reinterpret_cast<const uint32_t *>(buffer + i + 0x3c);
+            if (i + pe_offset + 4 < size) {
+                if (validate_pe_signature(buffer + i + pe_offset, size - i - pe_offset)) {
+                    pe_offsets.push_back(i + pe_offset);
+                }
             }
         }
     }
 }
 
-// Extract an executable section from the given buffer and write it to the given file
-void extract(const std::vector<char>& buffer, uint32_t pe_offset, const std::string& output_path) {
-    const char* dos_header = &buffer[0];
-    const char* pe_header = &buffer[pe_offset];
-
-    // Check for valid DOS header
-    if (memcmp(dos_header, "MZ\0\0", 4) != 0) {
-        std::cerr << "Invalid DOS header\n";
-        return;
-    }
-
-    // Check for valid Win32 or Win64 PE format
-    const char* pe_magic = &pe_header[0x18];
-    if (memcmp(pe_magic, "PE\0\0", 4) != 0) {
-        std::cerr << "Invalid PE header\n";
-        return;
-    }
-
-    uint16_t machine_type = *(uint16_t*)&pe_header[0x4];
-    uint32_t image_base = *(uint32_t*)&pe_header[0x34];
-
-    // Check for valid machine type
-    if (machine_type != IMAGE_FILE_MACHINE_I386 && machine_type != IMAGE_FILE_MACHINE_AMD64) {
-        std::cerr << "Invalid machine type\n";
-        return;
-    }
-
-    uint32_t header_size = *(uint16_t*)&pe_header[0x14];
-    uint16_t num_sections = *(uint16_t*)&pe_header[0x6];
-    uint32_t section_table_offset = pe_offset + 4 + 2 + *(uint32_t*)&pe_header[0x78];
-
-    // Find the code section and its properties
-    uint32_t code_base = image_base;
-    uint32_t code_size = 0;
-    uint32_t code_offset = 0;
-    for (uint16_t i = 0; i < num_sections; i++) {
-        uint32_t section_base = *(uint32_t*)&buffer[section_table_offset + 8*i];
-        uint32_t section_size = *(uint32_t*)&buffer[section_table_offset + 
+// Extract a single PE file from the memory dump
+void extract
