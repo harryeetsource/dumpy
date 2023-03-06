c98d5f446af71111913b9d47fe75f7facf5b3b26
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2023-03-06 14:38:06 -0800

    Update dumpy_beta.cpp

--- dumpy_beta.cpp
+++ dumpy_beta.cpp
@@ -12,104 +12,201 @@
 
 #include <windows.h>
 
+#include <thread>
+
+#include <mutex>
+
+#include <condition_variable>
+
 namespace fs = std::experimental::filesystem;
 
 void extract_executables(const std::string & input_path,
-  const std::string & output_path) {
-  std::ifstream input_file(input_path, std::ios::binary);
-  if (!input_file) {
-    std::cerr << "Failed to open input file: " << input_path << std::endl;
-    return;
-  }
+    const std::string & output_path, size_t start_pos, size_t end_pos) {
+    std::ifstream input_file(input_path, std::ios::binary);
+    if (!input_file) {
+      std::cerr << "Failed to open input file: " << input_path << std::endl;
+      return;
+    }
+    int count = 0;
 
-  std::vector < char > buffer(std::istreambuf_iterator < char > (input_file), {});
-  input_file.close();
+    // Set input file position to start of chunk.
+    input_file.seekg(start_pos);
 
-  const char * data = buffer.data();
-  const size_t size = buffer.size();
+    std::vector < char > buffer(end_pos - start_pos);
+    input_file.read(buffer.data(), buffer.size());
+    input_file.close();
 
-  size_t pos = 0;
-  int count = 0;
-  while (pos < size) {
-    // Search for DOS header.
-    const char * dos_header = & data[pos];
-    const char * dos_magic = "MZ";
-    const size_t dos_magic_size = 2; // DOS header is always 2 bytes long.
-    if (memcmp(dos_header, dos_magic, dos_magic_size) == 0) {
-      // Check for valid DOS executable format.
-      const uint16_t e_lfanew = * reinterpret_cast <
-        const uint16_t * > ( & data[pos + 0x3C]);
-      if (e_lfanew >= dos_magic_size && e_lfanew < size) {
-        const char * pe_header = & data[pos + e_lfanew];
+    const char * data = buffer.data();
+    const size_t size = buffer.size();
+
+    size_t pos = 0;
+
+    while (pos < size) {
+      // Search for DOS header.
+      const char * dos_header = & data[pos];
+      const char * dos_magic = "MZ";
+      const size_t dos_magic_size = 2; // DOS header is always 2 bytes long.
+      if (memcmp(dos_header, dos_magic, dos_magic_size) == 0) {
+        // Check for valid DOS executable format.
+        const char * pe_header = & data[pos + 0x3C];
+        const uint32_t pe_offset = * reinterpret_cast <
+          const uint32_t * > (pe_header);
+        const char * pe_signature = & data[pos + pe_offset];
         const char * pe_magic = "PE\0\0";
         const size_t pe_magic_size = 4; // PE signature is always 4 bytes long.
-        if (memcmp(pe_header, pe_magic, pe_magic_size) == 0) {
+        if (pe_offset != 0 && pos + pe_offset + pe_magic_size <= size &&
+          memcmp(pe_signature, pe_magic, pe_magic_size) == 0) {
           // Check for Win32 or Win64 PE format.
           const uint16_t pe_machine = * reinterpret_cast <
-            const uint16_t * > ( & pe_header[4]);
-          if (pe_machine == IMAGE_FILE_MACHINE_I386 || pe_machine == IMAGE_FILE_MACHINE_AMD64) {
-            const uint32_t pe_sections_count = * reinterpret_cast <
-              const uint16_t * > ( & pe_header[6]);
-            const uint16_t pe_size_of_optional_header = * reinterpret_cast <
-              const uint16_t * > ( & pe_header[20]);
-            const uint32_t pe_image_size = * reinterpret_cast <
-              const uint32_t * > ( & pe_header[80]);
-            const uint32_t pe_header_size = e_lfanew + pe_size_of_optional_header + 24 + (40 * pe_sections_count);
-            if (pos + pe_header_size + pe_image_size <= size) {
-              // Extract PE file data.
+            const uint16_t * > ( & data[pos + pe_offset + 0x4]);
+          if (pe_machine == 0x14c) { // Win32
+            uint32_t pe_size = * reinterpret_cast <
+              const uint32_t * > ( & data[pos + pe_offset + 0x50]);
+            if (pe_size != 0 && pos + pe_offset + pe_size <= size && pe_size <= 100000000) {
+              // Extract Win32 PE file data.
               const char * file_data = & data[pos];
               std::string filename = output_path + std::to_string(count) + ".exe";
               std::ofstream output_file(filename, std::ios::binary);
               if (output_file) {
-                output_file.write(file_data, pe_header_size + pe_image_size);
-
-                // Verify that the file has a valid ending and no MZ header is appended to the end.
-                uint16_t last_two_bytes = * reinterpret_cast <
-                  const uint16_t * > ( & data[pos + pe_header_size + pe_image_size - 2]);
-                if (last_two_bytes != 0 || memcmp( & data[pos + pe_header_size + pe_image_size - dos_magic_size], dos_magic, dos_magic_size) == 0) {
-                  std::cerr << "Invalid ending or MZ header appended to the end of the file: " << filename << std::endl;
-                  output_file.close();
-                  std::remove(filename.c_str());
-                  pos += pe_header_size + 1; // skip to next possible DOS header
-                  continue;
-                }
+                output_file.write(file_data, pe_size + pe_offset);
                 output_file.close();
                 std::cout << "Extracted file: " << filename << std::endl;
               } else {
                 std::cerr << "Failed to open output file: " << filename << std::endl;
               }
               count++;
-              pos += pe_header_size + pe_image_size;
-            } else {
-              std::cerr << "Invalid size or size too large: " << pe_image_size << std::endl;
-              pos += pe_header_size + 1; // skip to next possible DOS header
+              pos += pe_size + pe_offset;
               continue;
             }
-          } else {
-            std::cerr << "Invalid machine type: " << pe_machine << std::endl;
-            pos += e_lfanew + 1; // skip to next possible DOS header
-            continue;
+          } else if (pe_machine == 0x8664) { // Win64
+            uint32_t pe_size = * reinterpret_cast <
+              const uint32_t * > ( & data[pos + pe_offset + 0x50]);
+            if (pe_size != 0 && pos + pe_offset + pe_size <= size && pe_size <= 100000000) {
+              // Extract Win64 PE file data.
+              const char * file_data = & data[pos];
+              std::string filename = output_path + std::to_string(count) + ".exe";
+              std::ofstream output_file(filename, std::ios::binary);
+              if (output_file) {
+                output_file.write(file_data, pe_size + pe_offset);
+                output_file.close();
+                std::cout << "Extracted file: " << filename << std::endl;
+              } else {
+                std::cerr << "Failed to open output file: " << filename << std::endl;
+              }
+              count++;
+              pos += pe_size + pe_offset;
+              continue;
+            }
           }
         }
+        // Check for 32-bit executable.
+        const uint16_t magic = * reinterpret_cast <
+          const uint16_t * > ( & data[pos]);
+        if (magic == 0x5a4d) { // "MZ"
+          const uint32_t pe_offset = * reinterpret_cast <
+            const uint32_t * > ( & data[pos + 0x3c]);
+          const char * pe_signature = & data[pos + pe_offset];
+          const char * pe_magic = "PE\0\0";
+          const size_t pe_magic_size = 4; // PE signature is always 4 bytes long.
+          if (pe_offset != 0 && pos + pe_offset + pe_magic_size <= size &&
+            memcmp(pe_signature, pe_magic, pe_magic_size) == 0) {
+            const uint16_t pe_machine = * reinterpret_cast <
+              const uint16_t * > ( & data[pos + pe_offset + 0x4]);
+            if (pe_machine == 0x14c) { // Win32
+              uint32_t pe_size = * reinterpret_cast <
+                const uint32_t * > ( & data[pos + pe_offset + 0x50]);
+              if (pe_size != 0 && pos + pe_offset + pe_size <= size && pe_size <= 100000000) {
+                // Extract 32-bit PE file data.
+                const char * file_data = & data[pos];
+                std::string filename = output_path + std::to_string(count) + ".exe";
+                std::ofstream output_file(filename, std::ios::binary);
+                if (output_file) {
+                  output_file.write(file_data, pe_size + pe_offset);
+                  output_file.close();
+                  std::cout << "Extracted file: " << filename << std::endl;
+                } else {
+                  std::cerr << "Failed to open output file: " << filename << std::endl;
+                }
+                count++;
+                pos += pe_size + pe_offset;
+                continue;
+              }
+            } else if (pe_machine == 0x8664) { // Win64
+              uint32_t pe_size = * reinterpret_cast <
+                const uint32_t * > ( & data[pos + pe_offset + 0x50]);
+              if (pe_size != 0 && pos + pe_offset + pe_size <= size && pe_size <= 100000000) {
+                // Extract 64-bit PE file data.
+                const char * file_data = & data[pos];
+                std::string filename = output_path + std::to_string(count) + ".exe";
+                std::ofstream output_file(filename, std::ios::binary);
+                if (output_file) {
+                  output_file.write(file_data, pe_size + pe_offset);
+                  output_file.close();
+                  std::cout << "Extracted file: " << filename << std::endl;
+                } else {
+                  std::cerr << "Failed to open output file: " << filename << std::endl;
+                }
+                count++;
+                pos += pe_size + pe_offset;
+                continue;
+              }
+            }
+          }
+        }
+        std::cout << "Processed chunk from " << "[" << start_pos << ", " << end_pos << ")" << std::endl;
       }
     }
-    pos++;
-  }
+    }
+    int main(int argc, char * argv[]) {
+      if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " input_file output_dir" << std::endl;
+        return 1;
+      }
+
+      const std::string input_path = argv[1];
+      const std::string output_path = argv[2];
+
+      if (!fs::exists(input_path)) {
+        std::cerr << "Input file does not exist: " << input_path << std::endl;
+        return 1;
+      }
+
+      if (!fs::is_regular_file(input_path)) {
+        std::cerr << "Input path is not a regular file: " << input_path << std::endl;
+        return 1;
+      }
+
+      if (!fs::exists(output_path)) {
+        if (!fs::create_directory(output_path)) {
+          std::cerr << "Failed to create output directory: " << output_path << std::endl;
+          return 1;
+        }
+      }
+
+      if (!fs::is_directory(output_path)) {
+        std::cerr << "Output path is not a directory: " << output_path << std::endl;
+        return 1;
+      }
+
+      const size_t num_threads = std::thread::hardware_concurrency();
+      const size_t chunk_size = fs::file_size(input_path) / num_threads;
+      std::vector<std::thread> threads(num_threads);
+      std::vector < size_t > start_positions(num_threads);
+      std::vector < size_t > end_positions(num_threads);
+
+      for (size_t i = 0; i < num_threads; ++i) {
+    start_positions[i] = i * chunk_size;
+    end_positions[i] = (i == num_threads - 1) ? fs::file_size(input_path) : start_positions[i] + chunk_size;
+    threads[i] = std::thread(extract_executables, input_path, output_path, start_positions[i], end_positions[i]);
+}
+for (auto & t: threads) {
+    t.join();
 }
 
-int main(int argc, char ** argv) {
-  if (argc != 3) {
-    std::cerr << "Usage: " << argv[0] << " input_file output_dir" << std::endl;
-    return 1;
-  }
 
-  const std::string input_path = argv[1];
-  const std::string output_path = argv[2];
-  if (!fs::exists(output_path)) {
-    fs::create_directory(output_path);
-  }
 
-  extract_executables(input_path, output_path);
 
-  return 0;
-}
+
+
+      return 0;
+    }
