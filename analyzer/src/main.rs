c165c47ab9e32f86830f8eed623943853f16f820
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2023-06-26 09:34:04 -0700

    Add files via upload

--- analyzer/src/main.rs
+++ analyzer/src/main.rs
@@ -1,224 +1,214 @@
-use std::env;
-use std::fs::File;
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs::{self, File};
+use std::io::{Write};
 use std::path::Path;
+use std::str;
 use byteorder::{ByteOrder, LittleEndian};
-use core::cmp::min;
-use std::io::{self, Read, Write};
-use std::collections::{HashSet, VecDeque, HashMap};
+use std::borrow::Borrow;
+use std::ffi::CStr;
+use std::borrow::Cow;
+struct MZHeader {
+    Signature: u16,
+    LastPageSize: u16,
+    Pages: u16,
+    Relocations: u16,
+    HeaderSize: u16,
+    MinAlloc: u16,
+    MaxAlloc: u16,
+    InitialSS: u16,
+    InitialSP: u16,
+    Checksum: u16,
+    InitialIP: u16,
+    InitialCS: u16,
+    RelocAddr: u16,
+    OverlayNum: u16,
+    Reserved: [u16; 8],
+    OEMID: u16,
+    OEMInfo: u16,
+    Reserved2: [u16; 20],
+    PEHeaderAddr: u32,
+}
 
-fn calculate_entropy(data: &[u8]) -> f64 {
-    let length = data.len() as f64;
-    if length == 0.0 {
-        return 0.0;
-    }
+struct PEHeader {
+    Signature: u32,
+    Machine: u16,
+    NumberOfSections: u16,
+    TimeDateStamp: u32,
+    PointerToSymbolTable: u32,
+    NumberOfSymbols: u32,
+    SizeOfOptionalHeader: u16,
+    Characteristics: u16,
+}
 
-    let mut occurrence: [u32; 256] = [0; 256];
-    for &value in data {
-        occurrence[value as usize] += 1;
-    }
+struct PESectionHeader {
+    Name: [u8; 8],
+    VirtualSize: u32,
+    VirtualAddress: u32,
+    SizeOfRawData: u32,
+    PointerToRawData: u32,
+    PointerToRelocations: u32,
+    PointerToLinenumbers: u32,
+    NumberOfRelocations: u16,
+    NumberOfLinenumbers: u16,
+    Characteristics: u32,
+}
 
-    let mut entropy = 0.0;
-    for &value in occurrence.iter() {
-        let p_x = f64::from(value) / length;
-        if p_x > 0.0 {
-            entropy -= p_x * p_x.log2();
+fn find_pe_offset(data: &[u8], pos: usize) -> Option<usize> {
+    let min_pe_offset = 0x40;
+    let max_pe_offset = 0x200;
+
+    for offset in min_pe_offset..=max_pe_offset {
+        if pos + offset + 4 > data.len() {
+            break;
+        }
+        if data[pos + offset..pos + offset + 4] == [0x50, 0x45, 0x00, 0x00] {
+            return Some(offset);
         }
     }
 
-    entropy
+    None
 }
-
-fn find_mz_headers(data: &[u8]) -> Vec<usize> {
-    let mut mz_offsets = Vec::new();
-    for i in 0..data.len() - 1 {
-        if data[i..i+2] == [0x4D, 0x5A] {
-            mz_offsets.push(i);
+fn extract_executables(input_path: &str, output_path: &str) {
+    let data = match std::fs::read(input_path) {
+        Ok(data) => data,
+        Err(err) => {
+            eprintln!("Failed to read input file: {}", err);
+            return;
         }
-    }
-    mz_offsets
-}
-
-fn extract_executables(input_path: &str, output_path: &str) -> io::Result<()> {
-    let mut data = Vec::new();
-    File::open(input_path)?.read_to_end(&mut data)?;
-
-    let mut count = 0;
-    let mut headers = HashSet::new();
-    let mut extraction_queue = VecDeque::new();
+    };
 
     let mz_offsets = find_mz_headers(&data);
+    let mut count = 0;
+    let mut headers = HashMap::<Cow<str>, bool>::new();
 
     for pos in mz_offsets {
-        let pe_header_addr = LittleEndian::read_u32(&data[pos + 0x3C..pos + 0x3C + 4]) as usize;
+        let pe_header_addr = match read_u32_from_slice(&data[pos + 0x3C..pos + 0x3C + 4]) {
+            Some(addr) => addr as usize,
+            None => continue,
+        };
         let pe_header_pos = pos + pe_header_addr;
 
         if pe_header_addr <= 0 || pe_header_pos >= data.len() || pe_header_pos + 4 > data.len() {
             continue;
         }
 
-        if data[pe_header_pos..pe_header_pos + 4] == [0x50, 0x45, 0x00, 0x00] {
-            let pe_machine = LittleEndian::read_u16(&data[pe_header_pos + 4..pe_header_pos + 4 + 2]);
-            let number_of_sections = LittleEndian::read_u16(&data[pe_header_pos + 6..pe_header_pos + 6 + 2]);
-
-            if pe_machine == 0x14c || pe_machine == 0x8664 {
-                let magic = LittleEndian::read_u16(&data[pe_header_pos + 0x18..pe_header_pos + 0x18 + 2]);
-                let size_of_image_offset;
-                let size_of_initialized_data_offset;
-                let size_of_uninitialized_data_offset;
-
-                if magic == 0x20b {
-                    size_of_image_offset = 0x18;
-                    size_of_initialized_data_offset = 0x20;
-                    size_of_uninitialized_data_offset = 0x24;
-                } else {
-                    size_of_image_offset = 0x1C;
-                    size_of_initialized_data_offset = 0x28;
-                    size_of_uninitialized_data_offset = 0x2C;
-                }
-
-                let pe_size = LittleEndian::read_u32(&data[pe_header_pos + size_of_image_offset..pe_header_pos + size_of_image_offset + 4]) as usize;
-                let file_alignment = LittleEndian::read_u32(&data[pe_header_pos + size_of_initialized_data_offset..pe_header_pos + size_of_initialized_data_offset + 4]) as usize;
-                let size_of_image = LittleEndian::read_u32(&data[pe_header_pos + size_of_uninitialized_data_offset..pe_header_pos + size_of_uninitialized_data_offset + 4]) as usize;
-
-                let rounded_up_size_of_image = if file_alignment > 0 {
-                    ((size_of_image + file_alignment - 1) / file_alignment) * file_alignment
-                } else {
-                    size_of_image
-                };
-
-                if rounded_up_size_of_image != 0 && pe_header_pos + rounded_up_size_of_image <= data.len() && rounded_up_size_of_image <= 100000000 {
-                    let header_str = String::from_utf8_lossy(&data[pe_header_pos..pe_header_pos + min(1024, pe_size)]);
-
-                    if !headers.contains(&header_str) {
-                        headers.insert(header_str.to_owned());
-
-                        let extracted_filename = format!("{}/{}.exe", output_path, count);
-                        let exe_data = &data[pos..pos + rounded_up_size_of_image];
-                        File::create(&extracted_filename)?.write_all(exe_data)?;
-                        println!("Extracted file: {}", extracted_filename);
-                        count += 1;
-
-                        extraction_queue.push_back((pe_header_pos, pe_size));
-                    }
-                }
-            }
-        }
-    }
-
-    let mut extracted_files = HashSet::new();
-
-    while let Some((start_pos, pe_size)) = extraction_queue.pop_front() {
-        if pe_size <= 0 || start_pos + pe_size > data.len() {
+        if &data[pe_header_pos..pe_header_pos + 4] != b"PE\0\0" {
             continue;
         }
 
-        let embedded_data_slice = &data[start_pos..start_pos + pe_size];
-        let mut nested_extraction_queue = VecDeque::new();
-        nested_extraction_queue.push_back((start_pos, pe_size));
+        let pe_machine = match read_u16_from_slice(&data[pe_header_pos + 4..pe_header_pos + 4 + 2]) {
+            Some(machine) => machine,
+            None => continue,
+        };
 
-        while let Some((nested_start_pos, nested_pe_size)) = nested_extraction_queue.pop_front() {
-            let nested_mz_offsets = find_mz_headers(&embedded_data_slice);
-
-            for nested_pos in nested_mz_offsets {
-                let pe_header_addr = if nested_start_pos + nested_pos + 0x3C + 4 <= embedded_data_slice.len() {
-                    LittleEndian::read_u32(&embedded_data_slice[nested_start_pos + nested_pos + 0x3C..nested_start_pos + nested_pos + 0x3C + 4]) as usize
-                } else {
-                    continue;
-                };
-                
-                let pe_header_pos = nested_start_pos + nested_pos + pe_header_addr - 0x3C;
-
-                if pe_header_addr <= 0 || pe_header_pos >= nested_start_pos + nested_pe_size || pe_header_pos + 4 > nested_start_pos + nested_pe_size {
-                    continue;
-                }
-
-                if pe_header_pos + 4 <= embedded_data_slice.len() && embedded_data_slice[pe_header_pos..pe_header_pos + 4] == [0x50, 0x45, 0x00, 0x00] {
-                    let pe_machine = LittleEndian::read_u16(&embedded_data_slice[pe_header_pos + 4..pe_header_pos + 4 + 2]);
-                    let number_of_sections = LittleEndian::read_u16(&embedded_data_slice[pe_header_pos + 6..pe_header_pos + 6 + 2]);
-
-                    if pe_machine == 0x14c || pe_machine == 0x8664 {
-                        let magic = LittleEndian::read_u16(&embedded_data_slice[pe_header_pos + 0x18..pe_header_pos + 0x18 + 2]);
-                        let size_of_image_offset;
-                        let size_of_initialized_data_offset;
-                        let size_of_uninitialized_data_offset;
-
-                        if magic == 0x20b {
-                            size_of_image_offset = 0x18;
-                            size_of_initialized_data_offset = 0x20;
-                            size_of_uninitialized_data_offset = 0x24;
-                        } else {
-                            size_of_image_offset = 0x1C;
-                            size_of_initialized_data_offset = 0x28;
-                            size_of_uninitialized_data_offset = 0x2C;
-                        }
-
-                        let pe_size = LittleEndian::read_u32(&embedded_data_slice[pe_header_pos + size_of_image_offset..pe_header_pos + size_of_image_offset + 4]) as usize;
-                        let file_alignment = LittleEndian::read_u32(&embedded_data_slice[pe_header_pos + size_of_initialized_data_offset..pe_header_pos + size_of_initialized_data_offset + 4]) as usize;
-                        let size_of_image = LittleEndian::read_u32(&embedded_data_slice[pe_header_pos + size_of_uninitialized_data_offset..pe_header_pos + size_of_uninitialized_data_offset + 4]) as usize;
-
-                        let rounded_up_size_of_image = if file_alignment > 0 {
-                            ((size_of_image + file_alignment - 1) / file_alignment) * file_alignment
-                        } else {
-                            size_of_image
-                        };
-
-                        if rounded_up_size_of_image != 0 && pe_header_pos + rounded_up_size_of_image <= nested_start_pos + nested_pe_size && rounded_up_size_of_image <= 100000000 {
-                            let header_str = String::from_utf8_lossy(&embedded_data_slice[pe_header_pos..pe_header_pos + min(1024, pe_size)]);
-
-                            if !headers.contains(&header_str) {
-                                headers.insert(header_str.to_owned());
-
-                                let extracted_filename = format!("{}/{}.exe", output_path, count);
-                                let exe_data = &embedded_data_slice[nested_pos..nested_pos + rounded_up_size_of_image];
-                                File::create(&extracted_filename)?.write_all(exe_data)?;
-                                println!("Extracted file: {}", extracted_filename);
-                                count += 1;
-
-                                nested_extraction_queue.push_back((start_pos + nested_pos, pe_size));
-                            }
-                        }
-                    }
-                }
-            }
+        if pe_machine != 0x14c && pe_machine != 0x8664 {
+            continue;
         }
 
-        // Mark the current extracted file as complete
-        extracted_files.insert((start_pos, pe_size));
+        let pe_size = match read_u32_from_slice(&data[pe_header_pos + 0x50..pe_header_pos + 0x50 + 4]) {
+            Some(size) => size as usize,
+            None => continue,
+        };
+        let file_alignment = match read_u32_from_slice(&data[pe_header_pos + 0x3C..pe_header_pos + 0x3C + 4]) {
+            Some(alignment) => alignment as usize,
+            None => continue,
+        };
+
+        if pe_size == 0 || pe_header_pos + pe_size > data.len() || pe_size > 100_000_000 {
+            continue;
+        }
+
+        let header_bytes = &data[pe_header_pos..pe_header_pos + pe_size];
+        let header_str = String::from_utf8_lossy(header_bytes);
+
+        if headers.contains_key::<Cow<str>>(header_str.borrow()) {
+            continue;
+        }
+
+        headers.insert(header_str.into_owned().into(), true);
+
+        let padding = if file_alignment != 0 && pe_size % file_alignment != 0 {
+            file_alignment - (pe_size % file_alignment)
+        } else {
+            0
+        };
+
+        let extracted_size = pe_size + padding;
+        if pe_header_pos + extracted_size <= data.len() {
+            let filename = format!("{}{}.exe", output_path, count);
+            count += 1;
+
+            match std::fs::write(&filename, &data[pos..pos + extracted_size]) {
+                Ok(_) => println!("Extracted file: {}", filename),
+                Err(err) => eprintln!("Failed to write output file: {}", err),
+            }
+        }
     }
 
     if count == 0 {
-        println!("No executables found in the input file.");
+        println!("No executables found in input file.");
     } else {
-        println!("Extracted {} executables to the output path: {}", count, output_path);
+        println!("Extracted {} executables to output path: {}", count, output_path);
     }
-
-    Ok(())
 }
 
+fn read_u16_from_slice(slice: &[u8]) -> Option<u16> {
+    if slice.len() >= 2 {
+        Some(u16::from_le_bytes([slice[0], slice[1]]))
+    } else {
+        None
+    }
+}
 
+fn read_u32_from_slice(slice: &[u8]) -> Option<u32> {
+    if slice.len() >= 4 {
+        Some(u32::from_le_bytes([slice[0], slice[1], slice[2], slice[3]]))
+    } else {
+        None
+    }
+}
 
+fn find_mz_headers(buffer: &[u8]) -> Vec<usize> {
+    let dos_magic = [b'M', b'Z'];
+    let mut mz_positions = Vec::new();
 
-fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
+    for pos in 0..buffer.len() - dos_magic.len() {
+        if buffer[pos..pos + dos_magic.len()] == dos_magic {
+            mz_positions.push(pos);
+        }
+    }
+
+    mz_positions
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
     if args.len() != 3 {
         eprintln!("Usage: {} <input_file> <output_dir>", args[0]);
-        return Ok(());
+        return;
     }
 
     let input_path = &args[1];
     let output_path = &args[2];
 
-    if !Path::new(input_path).exists() {
-        println!("Input file does not exist: {}", input_path);
-        return Ok(());
+    if !std::path::Path::new(input_path).exists() {
+        eprintln!("Input file does not exist: {}", input_path);
+        return;
     }
 
-    if !Path::new(output_path).exists() {
-        std::fs::create_dir(output_path)?;
-    } else if !Path::new(output_path).is_dir() {
-        println!("Output path is not a directory: {}", output_path);
-        return Ok(());
+    if !std::path::Path::new(output_path).exists() {
+        match std::fs::create_dir(output_path) {
+            Ok(_) => (),
+            Err(err) => {
+                eprintln!("Failed to create output directory: {}", err);
+                return;
+            }
+        }
+    } else if !std::path::Path::new(output_path).is_dir() {
+        eprintln!("Output path is not a directory: {}", output_path);
+        return;
     }
 
-    extract_executables(input_path, output_path)
+    extract_executables(input_path, output_path);
 }
