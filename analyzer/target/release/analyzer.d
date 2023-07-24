c8d348052be001f6b531469a7c45fcfc72c6cc37
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2023-07-24 12:28:32 -0700

    Delete analyzer directory

--- analyzer/target/release/analyzer.d
+++
@@ -1,1 +0,0 @@
-C:\IT\rust\analyzer\target\release\analyzer.exe: C:\IT\rust\analyzer\src\main.rs
--- analyzer/target/release/deps/analyzer.d
+++
@@ -1,5 +0,0 @@
-C:\IT\rust\analyzer\target\release\deps\analyzer.exe: src\main.rs
-
-C:\IT\rust\analyzer\target\release\deps\analyzer.d: src\main.rs
-
-src\main.rs:
